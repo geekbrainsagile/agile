8a3cf974136c4c5735eb6ca299ce29a7ecd6df91
malashenok <a.malashenok@inbox.ru>
2021-01-07 00:03:49 +0300

    blade added

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,45 @@
+<li class="nav-item {{ request()->routeIs('Home') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('home') }}">{{ __('Главная') }}</a>
+</li>
+<li class="nav-item {{ request()->routeIs('News') ? 'active' : '' }}">
+    <a class="nav-link" href="#">{{ __('Новости') }}</a>
+</li>
+<li class="nav-item {{ request()->routeIs('Category') ? 'active' : '' }}">
+    <a class="nav-link" href="#">{{ __('Категории новостей') }}</a>
+</li>
+@if (!empty(Auth::user()->isAdmin))
+    <li class="nav-item dropdown {{ request()->routeIs('admin.news.index') ? 'active' : '' }}">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+           role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ __('Администрирование') }}
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <h6 class="dropdown-header">Новости</h6>
+            <a class="dropdown-item" href="#">
+                {{ __('Загрузка') }}
+            </a>
+            <a class="dropdown-item" href="#">
+                {{ __('Редактирование') }}
+            </a>
+            <a class="dropdown-item" href="#">
+                {{ __('Добавление') }}
+            </a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Категории</h6>
+            <a class="dropdown-item" href="#">
+                {{ __('Редактирование') }}
+            </a>
+            <a class="dropdown-item" href="#">
+                {{ __('Добавление') }}
+            </a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Источники</h6>
+            <a class="dropdown-item" href="#">
+                {{ __('Редактирование') }}
+            </a>
+            <a class="dropdown-item" href="#">
+                {{ __('Добавление') }}
+            </a>
+        </div>
+    </li>
+@endif
