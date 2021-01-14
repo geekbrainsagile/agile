e28e59d2817695ec6d62ca60ff0088003e435321
Alexey Tatarincev <tatarincev.alexey@adv-cake.ru>
2021-01-14 10:20:42 +0300

    разобраться с валидацией

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -70,23 +70,16 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                @if (Auth::user()->isAdmin)
-                                    <a class="dropdown-item" href="#">
-                                        {{ __('Профили пользователей') }}
-                                    </a>
-                                @else
-                                    <a class="dropdown-item" href="#">
-                                        {{ __('Профиль') }}
-                                    </a>
-                                @endif
-                                <a class="dropdown-item" href="#"
+                                <img src="{{empty(Auth::user()->avatar) ? Storage::url('public/img/no-avatar.png') : Auth::user()->avatar }}" alt="photo" class="dropdown-item pb-1 img-thumbnail avatar">
+                                <a class="dropdown-item" href="{{ route('profile.index', Auth::user()) }}">Профиль</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Выйти') }}
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
                             </div>
